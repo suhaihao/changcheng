dbb2f4dfc988e19fbab8b3726214e3fa536c2009
suhai <505604843@qq.com>
2020-05-12 14:42:51 +0800

    实体类修改

--- wydk/src/main/java/com/cc/wydk/entity/ActivityClock.java
+++ wydk/src/main/java/com/cc/wydk/entity/ActivityClock.java
@@ -14,4 +14,32 @@ public class ActivityClock {
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
+    @Column(name = "activity_id")
+    private Integer activityId;
+
+    @Column(name = "user_id")
+    private Integer userId;
+
+    @Column(name = "start_time")
+    private LocaDateTime startTime;
+
+    @Column(name = "end_time")
+    private LocaDateTime endTime;
+
+    @Column(name = "update_time")
+    private LocaDateTime updateTime;
+
+    @Column(name = "create_time")
+    private LocaDateTime createTime;
+
+    @Column(name = "duration")
+    private String duration;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "sign_up")
+    private String signUp;
+
+
 }
