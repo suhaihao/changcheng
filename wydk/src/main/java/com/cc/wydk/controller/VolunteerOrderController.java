08943a51940bc9cb8156e23520b0f22a5763479b
suhai <505604843@qq.com>
2020-06-09 11:44:44 +0800

    提交

--- wydk/src/main/java/com/cc/wydk/controller/VolunteerOrderController.java
+++ wydk/src/main/java/com/cc/wydk/controller/VolunteerOrderController.java
@@ -71,4 +71,15 @@ public class VolunteerOrderController {
         return volunteerOrderService.getById(request.getId());
     }
 
+    @PostMapping("/joinOrder")
+    @ApiOperation(value = "接单")
+    public Boolean joinOrder(@Valid @RequestBody VolunteerOrderDetailRequest request) {
+        VolunteerOrder byId = volunteerOrderService.getById(request.getId());
+        if (null != byId) {
+            byId.setUserId(UserUtils.getUserId());
+            return volunteerOrderService.updateById(byId);
+        }
+        return false;
+    }
+
 }
